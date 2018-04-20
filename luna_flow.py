9c0ef47eb71a63938b596ec44213306c35badb8b
Pål Grønås Drange <PGDR@statoil.com>
2018-04-20 09:44:05 +0200

    Add SPE3, extract flow

---
+++ luna_flow.py
@@ -0,0 +1,31 @@
+from ecl.summary import EclSumVarType, EclSum
+
+from parser import KEYS
+from luna_util import log
+
+# TODO
+#
+# Perhaps we only need F[OGW]P[RT] and should _fix_ the output to those
+#
+# Need to get wells, their type (injection/production) as well as report dates.
+
+def flow(state, step_idx, key):
+    """Takes a schedule and a step_idx and returns FOPR/FOPT for that step"""
+    now = state.schedule.timesteps[step_idx]
+    try:
+        nxt = state.schedule.timesteps[step_idx + 1]
+    except IndexError:
+        nxt = state. schedule.end
+    days = (nxt - now).days
+    wls = [w for w in state.schedule.wells if w.status(step_idx) == u'OPEN']
+    pros = [w for w in wls if w.isproducer(step_idx)]
+    injs = [w for w in wls if w.isinjector(step_idx)]
+
+    mockedval = len(pros)**2 * len(injs) * days  # yup
+
+    log(state, 'idx {} pros {} injs {} -> {}'.format(step_idx,
+                                                     pros,
+                                                     injs,
+                                                     mockedval))
+
+    return mockedval
--- summary.py
+++ summary.py
@@ -1,10 +1,41 @@
 from ecl.summary import EclSumVarType, EclSum
 
+from parser import KEYS
+from luna_flow import flow
+from luna_util import log
+
+
 # TODO
 #
 # Perhaps we only need F[OGW]P[RT] and should _fix_ the output to those
 #
 # Need to get wells, their type (injection/production) as well as report dates.
+
+
+def _extract_var_list(keys, smry):
+    var_list = []
+    for key in keys:
+        wgname = None
+        num = -1
+        var_type = EclSum.var_type(key)
+        if var_type in (EclSumVarType.ECL_SMSPEC_WELL_VAR,
+                        EclSumVarType.ECL_SMSPEC_GROUP_VAR):
+            try:
+                tmp = key.split(':')
+                kw = tmp[0]
+                wgname = tmp[1]
+            except IndexError as err:
+                raise ValueError('Broken key {}: {}'.format(key, err))
+        elif var_type == EclSumVarType.ECL_SMSPEC_FIELD_VAR:
+            kw = key
+        else:
+            raise ValueError(
+                'Only field, well and group variables are allowed, not {}'.format(key))
+        var_list.append(smry.addVariable(kw, wgname=wgname, num=num))
+    return var_list
+
+
+
 
 
 def generate_summary(state):
@@ -30,7 +61,7 @@ def generate_summary(state):
 
         for var in var_list:
             key = var.getKey1()
-            t_step[key] = _mock(state, idx, key)
+            t_step[key] = flow(state, idx, key)
 
         log(state, t_step)
 
