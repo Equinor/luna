697e2d50054afcb38a99082aa247780dcead133c
Lars Petter Øren Hauge <lpha@statoil.com>
2018-04-20 15:42:39 +0200

    Include injectors

--- python/luna/luna_flow.py
+++ python/luna/luna_flow.py
@@ -2,18 +2,18 @@ from ecl.summary import EclSumVarType, EclSum
 
 from .luna_util import log
 
-RESERVOIR = {}
+RESERVOIR_PROD = {}
+RESERVOIR_INJ = set()
 
-
-def accumulate(state, x, y, z, collect=1.0, n=3, visited=None):
+def accumulate(state, x, y, z, collect=1.0, n=10, visited=None):
     """Recursively collect oil"""
     if collect < 0.1 or n <= 0:
         return 0.
     if min(x, y, z) < 0 or x >= state.nx or y >= state.ny or z >= state.nz:
         return 0.
     gidx = state.gidx[(x, y, z)]
-    if gidx in RESERVOIR:
-        return RESERVOIR[gidx]
+    if gidx in RESERVOIR_PROD:
+        return RESERVOIR_PROD[gidx]
 
     if visited is None:
         visited = set()
@@ -41,11 +41,36 @@ def accumulate(state, x, y, z, collect=1.0, n=3, visited=None):
     oip += accumulate(
         state, x, y, z - 1, collect=collect * state.perm[2][gidx], n=n-1, visited=visited)
 
-    RESERVOIR[gidx] = oip
+    RESERVOIR_PROD[gidx] = oip
     return oip
 
+def mod_res(state, x, y, z, n=10, visited=None):
+    if n <=0:
+        return 0.
+    if min(x, y, z) < 0 or x >= state.nx or y >= state.ny or z >= state.nz:
+        return 0.
+    gidx = state.gidx[(x, y, z)]
+    if gidx in RESERVOIR_INJ:
+        return
 
+    if visited is None:
+        visited = set()
 
+    if gidx in visited:
+        return
+
+    visited.add(gidx)
+    state.perm[0][gidx] = state.perm[0][gidx] * 1.2
+    state.perm[1][gidx] = state.perm[1][gidx] * 1.2
+    state.perm[2][gidx] = state.perm[2][gidx] * 1.2
+
+    mod_res (state, x + 1, y, z, n - 1, visited)
+    mod_res (state, x - 1, y, z, n - 1, visited)
+    mod_res (state, x, y + 1, z, n - 1, visited)
+    mod_res (state, x, y - 1, z, n - 1, visited)
+    mod_res (state, x, y, z + 1, n - 1, visited)
+    mod_res (state, x, y, z - 1, n - 1, visited)
+    RESERVOIR_INJ.add(gidx)
 
 def flow(state, step_idx, key):
     """Takes a schedule and a step_idx and returns FOPR/FOPT for that step"""
@@ -54,6 +79,10 @@ def flow(state, step_idx, key):
     pcomp, icomp = state.completions[step_idx]
 
     oip = 0
+    for well in icomp:
+        for comp in well:
+            mod_res(state,*comp)
+
     for well in pcomp:
         for comp in well:
             oip += accumulate(state, *comp)
